6838a385272b7f261cbb8a5c036892d10d004298
Kukoverov.Dmitry <moby_dik@mail.ru>
2025-08-26 14:10:05 +0300

    создание окна

--- CATaas.py
+++ CATaas.py
@@ -1,2 +1,26 @@
 from tkinter import *
-print(1)
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+
+
+window = Tk()
+window.title("Cataas")
+window.geometry("600x480")
+
+
+label = Label()
+label.pack()
+
+
+url = "http://cataas.com/cat   "
+img = load_image(url)
+
+if img:
+    label.configure(image=img)
+    label.image = img
+
+
+window.mainloop()
+
