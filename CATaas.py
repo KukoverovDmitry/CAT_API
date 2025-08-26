fdd00715a882d017349c2276e19118ba1a6882c0
Kukoverov.Dmitry <moby_dik@mail.ru>
2025-08-26 14:42:21 +0300

    Добавлено меню

--- CATaas.py
+++ CATaas.py
@@ -9,7 +9,7 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
-        img.thumbnail((600,480), Image.Resample.LANCZOS)
+        img.thumbnail((600,480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as error:
         print(f"Ошибка {error}")
@@ -22,6 +22,9 @@ def set_image():
         label.configure(image=img)
         label.image = img
 
+def exit():
+    window.destroy()
+
 
 window = Tk()
 window.title("Cataas")
@@ -30,8 +33,19 @@ window.geometry("600x520")
 
 label = Label()
 label.pack()
-update_button = Button(text="Обновить", command= set_image)
-update_button.pack()
+# update_button = Button(text="Обновить", command= set_image)
+# update_button.pack()
+
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Загрузить фот", command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=exit)
+
+
+
 
 
 url = "https://cataas.com/cat"
